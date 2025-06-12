4c35b47253f0ba05ed4c00b1ac9e212ef2ef1394
emrepbu <emrewindowsarg@gmail.com>
2025-06-12 15:48:13 +0300

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by emre argana on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
