1d18cba9cef7efafb055518042dc348069250cff
emrepbu <emrewindowsarg@gmail.com>
2025-06-13 00:39:48 +0300

    Update ContentView.swift

--- MenuApp/ContentView.swift
+++ MenuApp/ContentView.swift
@@ -3,7 +3,7 @@ import Foundation
 
 struct ContentView: View {
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -23,17 +23,38 @@ struct ContentView: View {
 struct ItemRow: View {
     var item: MenuItem
     
+    let colors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green
+    ]
+    
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.green, lineWidth: 1))
             
             VStack(alignment: .leading) {
                 Text(item.name)
                 Text("\(item.price, format: .currency(code: "TRY"))")
             }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundStyle(.white)
+            }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-        .border(.green)
     }
 }
 
